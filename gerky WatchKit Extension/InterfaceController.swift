497bd9dd914cba249977cb549872b351302e13df
rozyckj <jakub.rozycki@myjar.com>
2021-08-18 12:23:02 +0200

    Gerky - shared state concept

---
+++ gerky WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  gerky WatchKit Extension
+//
+//  Created by rozyckj on 09/02/2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
